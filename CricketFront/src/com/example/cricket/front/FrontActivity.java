7f274be7edb23915bda50807033d801aa92dbb91
awais@tintash.com <awais@tintash.com>
2012-12-18 22:25:33 +0500

    removed errors

--- CricketFront/src/com/example/cricket/front/FrontActivity.java
+++ CricketFront/src/com/example/cricket/front/FrontActivity.java
@@ -1,22 +1,19 @@
 package com.example.cricket.front;
 
-import android.R;
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import com.example.cricket.front.*;
+import android.os.Bundle;
 
 public class FrontActivity extends Activity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_front, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(final Menu menu) {
+//        getMenuInflater().inflate(R.menu.activity_front, menu);
+//        return true;
+//    }
 }
